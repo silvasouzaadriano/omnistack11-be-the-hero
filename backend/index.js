df92606c8801704070458ede9e9d4a945552840a
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-04-02 19:41:28 -0300

    HTTP methods and Insomnia

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos HTTP:
+  * 
+  * GET: Buscar/list uma informação do backend
+  * POST: Criar uma informação do backend
+  * PUT: Alterar uma informação no backend
+  * DELETE: Deletar uma informação no backend
+  */
+
 const express = require('express');
 
 const app = express();
 
-app.get('', (request, response) => {
+app.get('/users', (request, response) => {
   return response.json({
     evento: 'Semana OmniStack 11.0',
     aluno: 'Adriano Souza'
